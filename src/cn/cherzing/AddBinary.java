c5209e6eff51d92b2dbe31b2ba4a0eacf0fead90
czlifetime <czmaxlifetime@qq.com>
2024-10-26 14:53:53 +0800

    二进制运算

--- src/cn/cherzing/AddBinary.java
+++ src/cn/cherzing/AddBinary.java
@@ -1,5 +1,6 @@
 package cn.cherzing;
 
+import javax.sound.midi.Soundbank;
 import java.math.BigInteger;
 
 /**
@@ -9,21 +10,18 @@ import java.math.BigInteger;
  */
 public class AddBinary {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        String string = solution.addBinaryBit("10111","1010");
-        String a = "0111";
-        System.out.println(string);
-        // System.out.println(s);
-        int aaa = a.charAt(0);
-        System.out.println(aaa);
-
+        SolutionAddBinary solution = new SolutionAddBinary();
+        String result = solution.addBinaryTest("1011", "101");
+        System.out.println(result);
     }
 }
-class Solution {
+
+class SolutionAddBinary {
     /**
      * 从字符串ab开始从后往前遍历，
      * 如果字符串相同位置之和=1，保持不变；
      * 如果字符串相同位置之和!=2，往前进一
+     *
      * @param a
      * @param b
      * @return
@@ -33,7 +31,7 @@ class Solution {
         int len2 = b.length();
         int max = Math.max(len1, len2);
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < max; i++){
+        for (int i = 0; i < max; i++) {
 
             int aI = a.charAt(max - i) - '0';
             int bI = b.charAt(max - i) - '0';
@@ -42,6 +40,7 @@ class Solution {
         String string = result.reverse().toString();
         return string;
     }
+
     public String addBinaryBit(String a, String b) {
         int n = a.length(), m = b.length();
         int max = Math.max(n, m);
@@ -49,13 +48,11 @@ class Solution {
         StringBuilder result = new StringBuilder();
 
         for (int i = 0; i < max; i++) {
-
-            int bitA = ( i < n ) ? a.charAt(n - 1 - i) - '0' : 0; //要么是1，要么是0
-            int bitB = ( i < m ) ? b.charAt(m - 1 - i) - '0' : 0; //要么是1，要么是0
+            int bitA = (i < n) ? a.charAt(n - 1 - i) - '0' : 0;
+            int bitB = (i < m) ? b.charAt(m - 1 - i) - '0' : 0;
             int sum = bitA + bitB + carry;
             result.append(sum & 1); // 添加当前位的结果
             carry = sum >> 1; // 计算新的进位
-
         }
 
         if (carry != 0) {
@@ -72,6 +69,64 @@ class Solution {
         return sum.toString(2);
     }
 
-    //双指针方法
 
+    public String addBinaryTest(String a, String b) {
+        int aIndex = a.length() - 1;// 2
+        int bIndex = b.length() - 1;// 3
+        int maxIndex = Math.max(aIndex, bIndex); // 3
+        int sub = Math.abs(aIndex - bIndex);
+        StringBuilder stringBuilderA = new StringBuilder(a);
+        StringBuilder stringBuilderB = new StringBuilder(b);
+        StringBuilder result = new StringBuilder();
+
+        //如果两个字符串长度不一，补零
+        if (aIndex > bIndex) {
+            for (int i = 0; i < sub; i++) {
+                stringBuilderB.insert(0, '0');
+            }
+        } else {
+            for (int i = 0; i < sub; i++) {
+                stringBuilderA.insert(0, '0');
+            }
+        }
+        int carry = 0;
+        int sum = 0;
+        //遍历每个字符串，上下两位相加
+        for (int i = maxIndex; i >= 0; i--) {
+            // 要么是 0，要么是 1
+            int bitA = stringBuilderA.charAt(i) - '0';
+            int bitB = stringBuilderB.charAt(i) - '0';
+            //相加
+            sum = bitA + bitB + carry; // 1+1+0
+            result.append(sum & 1); // 2 & 1 = 0
+
+            // 计算进位
+            carry = sum >> 1; // 2 >> 1
+
+        }
+        if (carry != 0){
+            result.append(carry);
+        }
+        return result.reverse().toString();
+    }
+
+    /**
+     * 不使用BigInteger
+     * 缺点： 超出范围
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public String addBinaryNB(String a, String b) {
+        //将字符串转为整数
+        int num1 = Integer.parseInt(a, 2);
+        int num2 = Integer.parseInt(b, 2);
+
+        // 相加
+        int sum = num1 + num2;
+
+        // 将结果转换回二进制字符串
+        return Integer.toBinaryString(sum);
+    }
 }
